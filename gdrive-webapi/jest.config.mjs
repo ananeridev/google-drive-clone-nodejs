474901bee64b31f71f1caf4a1e136dc7946df870
Ana Neri <ananeri32@hotmail.com>
2023-06-05 22:15:34 -0300

    config: test jest files
    Signed-off-by: Ana Neri <ananeri32@hotmail.com>

---
+++ gdrive-webapi/jest.config.mjs
@@ -0,0 +1,33 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coverageReporters: [
+    "text",
+    "lcov"
+  ],
+  testEnviroment: "node",
+  coverageThreshould: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  watchPathIgnorePatterns: [
+    "node_modules"
+  ],
+  transformsIgnorePatterns: ["node_modules"],
+  collectCoverageFrom: [
+    "src/**/*.js", "!src/**/index.js"
+  ]
+
+};
